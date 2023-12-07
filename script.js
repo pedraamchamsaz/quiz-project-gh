b75a56d9c6c94abcd868751cfb3d505392b1fc52
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2023-12-07 15:56:03 +0000

    remaining questions added

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const introPage = document.getElementById('intro');
 const question1 = document.getElementById('question1');
-const question2 = document.getElementById('question2');
+const nextButton = document.getElementById('next-btn');
 
 function startQuiz() {
     introPage.classList.add('hidden');
     question1.classList.remove('hidden');
+}
+
+function selectAnswer() {
+    nextButton.classList.remove('hidden');
 }
 
 let i = 1;
@@ -14,6 +18,7 @@ function loadNextQuestion() {
     let nextQuestionID = "question" + String(i + 1);
     let currentQuestion = document.getElementById(currentQuestionID);
     let nextQuestion = document.getElementById(nextQuestionID);
+    nextButton.classList.add('hidden');
     currentQuestion.classList.add('hidden');
     nextQuestion.classList.remove('hidden');
     i++;
